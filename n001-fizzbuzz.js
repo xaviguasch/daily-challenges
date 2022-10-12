ba13ff8155d6aa80b5507339e92cb949ccbf1648
xaviguasch <xaviguasch@gmail.com>
2022-10-12 18:42:30 +0200

    added challenge n001-fizzbuzz

---
+++ n001-fizzbuzz.js
@@ -0,0 +1,11 @@
+// Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 3, print out “Fizz”; when it’s divisible by 5, print out “Buzz”; when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
+
+// Hint: Loop with conditionals that use modulus
+
+const fizzBuzz = (number) => {
+  return
+}
+
+console.log(fizzBuzz(5))
+console.log(fizzBuzz(16))
+console.log(fizzBuzz(28))
