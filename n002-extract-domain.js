a554e428eabb1c61e080b10b619924d85ace2661
xaviguasch <xaviguasch@gmail.com>
2022-10-13 21:27:19 +0200

    added challenge n002-extact-domain

---
+++ n002-extract-domain.js
@@ -0,0 +1,13 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github"
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet
+
+const extractDomain = (domain) => {
+  return
+}
+
+console.log(extractDomain('http://github.com/carbonfive/raygun'))
+console.log(extractDomain('http://www.zombie-bites.com'))
+console.log(extractDomain('https://www.cnet.com"'))
