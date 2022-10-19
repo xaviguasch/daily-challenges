e6b14d4f7c1d5999a93b3ef60decd38909d9db38
xaviguasch <xaviguasch@gmail.com>
2022-10-19 20:21:22 +0200

    added solution to n003-sum-mixed-array

--- n003-sum-mixed-array.js
+++ n003-sum-mixed-array.js
@@ -6,8 +6,8 @@
 // assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42);
 // assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41);
 
-const sumMix = (x) => {
-  return
+const sumMix = (arr) => {
+  return arr.reduce((acc, currV) => acc + Number(currV), 0)
 }
 
 console.log(sumMix([9, 3, '7', '3']))
