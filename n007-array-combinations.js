d3d0410930c957237d09e19e4c19eafe2a6c742c
xaviguasch <xaviguasch@gmail.com>
2022-10-25 19:50:08 +0200

    added n007-array-combinations challenge

---
+++ n007-array-combinations.js
@@ -0,0 +1,36 @@
+// In this Kata, you will be given an array of arrays and your task will be to return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+// For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+// Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+// See test cases for more examples.
+
+// Good luck!
+
+const solve = (arr) => {
+  return 0
+}
+
+console.log(solve([[1, 2], [4], [5, 6]]))
+console.log(
+  solve([
+    [1, 2],
+    [4, 4],
+    [5, 6, 6],
+  ])
+)
+console.log(
+  solve([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ])
+)
+console.log(
+  solve([
+    [1, 2, 3],
+    [3, 4, 6, 6, 7],
+    [8, 9, 10, 12, 5, 6],
+  ])
+)
