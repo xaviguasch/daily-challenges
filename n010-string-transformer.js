c40a704c9fd6fc4f3c09bdbdc3b6518a1be39cc8
xaviguasch <xaviguasch@gmail.com>
2022-11-01 23:51:18 +0100

    added standup challenge n010-string-transformer

---
+++ n010-string-transformer.js
@@ -0,0 +1,17 @@
+// DESCRIPTION:
+// Given a string, return a new string that has transformed based on the input:
+
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+// For example:
+
+// "Example Input" ==> "iNPUT eXAMPLE"
+// You may assume the input only contain English alphabet and spaces.
+
+const stringTransformer = (str) => {
+  return
+}
+
+console.log(stringTransformer('Example string'))
