6f77119ede7af9e4f255de73b193b30043048615
xaviguasch <xaviguasch@gmail.com>
2022-11-02 22:40:34 +0100

    added standup challenge n011-sort-by-last-char

---
+++ n011-sort-by-last-char.js
@@ -0,0 +1,13 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+const last = (x) => {
+  return
+}
+
+console.log(last('man i need a taxi up to ubud'))
+console.log(last('what time are we climbing up the volcano'))
+console.log(last('take me to semynak'))
