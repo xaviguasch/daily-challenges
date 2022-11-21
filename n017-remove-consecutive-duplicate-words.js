df681aacabd5a36c1850fb57256e29df6628349b
xaviguasch <xaviguasch@gmail.com>
2022-11-21 23:58:23 +0100

    added n017-remove-consecutive-duplicate-words challenge

---
+++ n017-remove-consecutive-duplicate-words.js
@@ -0,0 +1,15 @@
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+const removeConsecutiveDuplicates = () => {
+  return
+}
+
+console.log(
+  removeConsecutiveDuplicates(
+    'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+  )
+)
