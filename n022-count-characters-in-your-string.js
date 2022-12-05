9ede383079a3bece8c1d2a818255e11a9fd380c6
xaviguasch <xaviguasch@gmail.com>
2022-12-05 22:46:02 +0100

    added n022-count-characters-in-your-string challenge

---
+++ n022-count-characters-in-your-string.js
@@ -0,0 +1,11 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+const count = (string) => {
+  return
+}
+
+console.log(count('aba'))
+console.log(count('malignant'))
+console.log(count(''))
