cffd1774d8daf98906dc9d29180e934bd2b93ace
xaviguasch <xaviguasch@gmail.com>
2022-12-08 20:51:00 +0100

    added n024-max-character challenge

---
+++ n024-max-character.js
@@ -0,0 +1,7 @@
+// Given a string of characters, return the character that appears the most often.
+
+const maxCharacter = (str) => {
+  retrun
+}
+
+console.log(maxCharacter('Hello World!')) // Should return 'l'
