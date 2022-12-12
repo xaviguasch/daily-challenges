bd2eedfa4ed4140479bed85fc23e26ae99058d1e
xaviguasch <xaviguasch@gmail.com>
2022-12-13 00:14:19 +0100

    added solution to n025-contains-duplicate-leetcode challenge

--- n025-contains-duplicate-leetcode.js
+++ n025-contains-duplicate-leetcode.js
@@ -13,8 +13,19 @@
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
-const containsDuplicate = (nums) => {
-  return
+const containsDuplicate = (arr) => {
+  let numsMap = {}
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i]
+
+    if (numsMap[num]) {
+      return true
+    } else {
+      numsMap[num] = true
+    }
+  }
+  return false
 }
 
 console.log(containsDuplicate([1, 2, 3, 1]))
