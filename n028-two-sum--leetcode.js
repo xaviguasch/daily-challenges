5f99450ecab24e828ad255de3abeaf51e7cb5794
xaviguasch <xaviguasch@gmail.com>
2022-12-16 00:20:05 +0100

    added harder variation to n028-two-sum--leetcode

--- n028-two-sum--leetcode.js
+++ n028-two-sum--leetcode.js
@@ -20,14 +20,24 @@
 
 const twoSum = (nums, target) => {
   // brute force solution
-  for (let i = 0; i <= nums.length - 1; i++) {
-    for (let y = i + 1; y <= nums.length - 1; y++) {
-      if (nums[i] + nums[y] === target) {
-        return [i, y]
-      }
-    }
-  }
+  // for (let i = 0; i <= nums.length - 1; i++) {
+  //   for (let y = i + 1; y <= nums.length - 1; y++) {
+  //     if (nums[i] + nums[y] === target) {
+  //       return [i, y]
+  //     }
+  //   }
+  // }
 }
 
-console.log(twoSum([2, 7, 11, 15], 9)) // Output: [0,1]
-console.log(twoSum([3, 2, 4], 6)) // Output: [1,2]
+// console.log(twoSum([2, 7, 11, 15], 9)) // Output: [0,1]
+// console.log(twoSum([3, 2, 4], 6)) // Output: [1,2]
+
+// ALTERNATIVE MORE COMPLICATED VARIATION
+
+// Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
+
+const twoSumHard = (nums, target) => {
+  return
+}
+
+console.log(twoSum([1, 2, 2, 3, 4], 4)) // Should return [[2, 2], [3, 1]]
