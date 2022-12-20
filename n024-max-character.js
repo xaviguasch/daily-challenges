8ddad9ff2b87c9ea55cc3a11ce311ebcc5843509
xaviguasch <xaviguasch@gmail.com>
2022-12-20 22:58:46 +0100

    added solution to n024-max-character challenge

--- n024-max-character.js
+++ n024-max-character.js
@@ -1,7 +1,21 @@
 // Given a string of characters, return the character that appears the most often.
 
 const maxCharacter = (str) => {
-  retrun
+  let charMap = {},
+    count = 0,
+    maxChar = null
+
+  for (const char of str) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  for (const char in charMap) {
+    if (charMap[char] > count) {
+      count = charMap[char]
+      maxChar = char
+    }
+  }
+  return maxChar
 }
 
 console.log(maxCharacter('Hello World!')) // Should return 'l'
